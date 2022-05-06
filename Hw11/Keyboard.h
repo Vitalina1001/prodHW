b008271bde4075940af17fb5f732a1166b09e029
Vitalina1001 <101709483+Vitalina1001@users.noreply.github.com>
2022-05-06 20:21:36 +0400

    final

--- Hw11/Keyboard.h
+++
@@ -1,9 +0,0 @@
-#ifndef KEYBORD_H_
-#define KEYBORD_H_
-
-class Keyboard
-{
-	virtual void Key() = 0;
-};
-
-#endif 
--- Hw11/Screen.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCREEN_H_
-#define SCREEN_H_
-
-class Screen
-{
-	virtual void pixel() = 0;
-};
-
-#endif 
